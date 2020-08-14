cloudfoundry/multiapps-controller

Micrometer traces steps and overall deploy time
Jira: LMCROSSITXSADEPLOY-1840

multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/listeners/EndProcessListener.java
@@ -5,6 +5,7 @@ import javax.inject.Named;
 
 import org.cloudfoundry.multiapps.controller.api.model.Operation;
 import org.cloudfoundry.multiapps.controller.process.util.OperationInFinalStateHandler;
+import org.cloudfoundry.multiapps.controller.processes.metering.MicrometerNotifier;
 import org.flowable.engine.delegate.DelegateExecution;
 
 @Named("endProcessListener")
@@ -13,16 +14,19 @@ public class EndProcessListener extends AbstractProcessExecutionListener {
     private static final long serialVersionUID = 2L;
 
     private final OperationInFinalStateHandler eventHandler;
+    private MicrometerNotifier micrometerNotifier;
 
     @Inject
-    public EndProcessListener(OperationInFinalStateHandler eventHandler) {
+    public EndProcessListener(OperationInFinalStateHandler eventHandler, MicrometerNotifier micrometerNotifier) {
         this.eventHandler = eventHandler;
+        this.micrometerNotifier = micrometerNotifier;
     }
 
     @Override
     protected void notifyInternal(DelegateExecution execution) {
         if (isRootProcess(execution)) {
             eventHandler.handle(execution, Operation.State.FINISHED);
+            micrometerNotifier.recordEndProcessEvent(execution);
         }
     }
 

multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/listeners/ErrorProcessListener.java
@@ -7,6 +7,7 @@ import org.cloudfoundry.multiapps.controller.core.util.LoggingUtil;
 import org.cloudfoundry.multiapps.controller.process.util.OperationInErrorStateHandler;
 import org.cloudfoundry.multiapps.controller.process.variables.VariableHandling;
 import org.cloudfoundry.multiapps.controller.process.variables.Variables;
+import org.cloudfoundry.multiapps.controller.processes.metering.MicrometerNotifier;
 import org.flowable.common.engine.api.delegate.event.FlowableEngineEntityEvent;
 import org.flowable.common.engine.api.delegate.event.FlowableEngineEvent;
 import org.flowable.common.engine.api.delegate.event.FlowableExceptionEvent;
@@ -22,10 +23,12 @@ public class ErrorProcessListener extends AbstractFlowableEngineEventListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(ErrorProcessListener.class);
 
     private final OperationInErrorStateHandler eventHandler;
+    private MicrometerNotifier micrometerNotifier;
 
     @Inject
-    public ErrorProcessListener(OperationInErrorStateHandler eventHandler) {
+    public ErrorProcessListener(OperationInErrorStateHandler eventHandler, MicrometerNotifier micrometerNotifier) {
         this.eventHandler = eventHandler;
+        this.micrometerNotifier = micrometerNotifier;
     }
 
     @Override
@@ -71,12 +74,15 @@ public class ErrorProcessListener extends AbstractFlowableEngineEventListener {
 
     private void handle(FlowableEngineEvent event, FlowableExceptionEvent exceptionEvent) {
         Throwable throwable = exceptionEvent.getCause();
+        DelegateExecution execution = getExecution(event);
         if (throwable == null) {
             LOGGER.error("Job execution failure detected for process \"{}\" (definition: \"{}\"), but the exception event does not contain an exception.",
                          event.getProcessInstanceId(), event.getProcessDefinitionId());
+            micrometerNotifier.recordErrorProcessEvent(execution, null);
         } else {
             LOGGER.error("Job execution failure detected for process \"{}\" (definition: \"{}\").", event.getProcessInstanceId(),
                          event.getProcessDefinitionId(), throwable);
+            micrometerNotifier.recordErrorProcessEvent(execution, throwable.getMessage());
             eventHandler.handle(event, throwable);
         }
     }

multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/util/OperationTimeAggregator.java
@@ -8,9 +8,14 @@ import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.cloudfoundry.multiapps.controller.api.model.Operation;
 import org.cloudfoundry.multiapps.controller.process.flowable.FlowableFacade;
 import org.cloudfoundry.multiapps.controller.process.util.ProcessTimeCalculator.ProcessTime;
 import org.cloudfoundry.multiapps.controller.process.util.ProcessTimeCalculator.ProcessTimeLogger;
+import org.cloudfoundry.multiapps.controller.process.variables.VariableHandling;
+import org.cloudfoundry.multiapps.controller.process.variables.Variables;
+import org.cloudfoundry.multiapps.controller.processes.metering.MicrometerNotifier;
+import org.flowable.engine.delegate.DelegateExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,14 +26,17 @@ public class OperationTimeAggregator {
 
     private ProcessTimeCalculator processTimeCalculator;
     private FlowableFacade flowableFacade;
+    private MicrometerNotifier micrometerNotifier;
 
     @Inject
-    public OperationTimeAggregator(FlowableFacade flowableFacade) {
+    public OperationTimeAggregator(FlowableFacade flowableFacade, MicrometerNotifier micrometerNotifier) {
         this.flowableFacade = flowableFacade;
         this.processTimeCalculator = new ProcessTimeCalculator(flowableFacade);
+        this.micrometerNotifier = micrometerNotifier;
     }
 
-    public void aggregateOperationTime(String correlationId) {
+    public void aggregateOperationTime(DelegateExecution execution, Operation.State state) {
+        String correlationId = VariableHandling.get(execution, Variables.CORRELATION_ID);
         List<String> historicSubProcesses = flowableFacade.getHistoricSubProcessIds(correlationId);
         historicSubProcesses.add(correlationId);
 
@@ -39,6 +47,7 @@ public class OperationTimeAggregator {
                 key));
 
         ProcessTime rootProcessTime = processTimesForSubProcesses.get(correlationId);
+        micrometerNotifier.recordOverallTime(execution, state, rootProcessTime.getProcessDuration());
         logOverallProcesstime(correlationId, rootProcessTime, processTimesForSubProcesses.values());
     }
 

multiapps-controller-process/src/test/java/org/cloudfoundry/multiapps/controller/process/listeners/ErrorProcessListenerTest.java
@@ -5,6 +5,7 @@ import org.cloudfoundry.multiapps.controller.process.util.OperationInErrorStateH
 import org.cloudfoundry.multiapps.controller.process.util.ProcessTimeCalculator;
 import org.cloudfoundry.multiapps.controller.process.variables.VariableHandling;
 import org.cloudfoundry.multiapps.controller.process.variables.Variables;
+import org.cloudfoundry.multiapps.controller.processes.metering.MicrometerNotifier;
 import org.flowable.common.engine.api.delegate.event.FlowableEngineEntityEvent;
 import org.flowable.common.engine.api.delegate.event.FlowableEngineEvent;
 import org.flowable.common.engine.api.delegate.event.FlowableExceptionEvent;
@@ -24,8 +25,8 @@ public class ErrorProcessListenerTest {
 
     private static class ErrorProcessListenerMock extends ErrorProcessListener {
 
-        public ErrorProcessListenerMock(OperationInErrorStateHandler eventHandler) {
-            super(eventHandler);
+        public ErrorProcessListenerMock(OperationInErrorStateHandler eventHandler, MicrometerNotifier micrometerNotifier) {
+            super(eventHandler, micrometerNotifier);
         }
 
         protected DelegateExecution execution;
@@ -39,6 +40,9 @@ public class ErrorProcessListenerTest {
 
     @Mock
     private OperationInErrorStateHandler eventHandler;
+    
+    @Mock
+    private MicrometerNotifier micrometerNotifier;
 
     @Mock
     private ProcessTimeCalculator processTimeCalculator;

multiapps-controller-process/src/test/java/org/cloudfoundry/multiapps/controller/process/listeners/StartProcessListenerTest.java
@@ -25,6 +25,7 @@ import org.cloudfoundry.multiapps.controller.process.util.ProcessTypeParser;
 import org.cloudfoundry.multiapps.controller.process.util.StepLogger;
 import org.cloudfoundry.multiapps.controller.process.variables.VariableHandling;
 import org.cloudfoundry.multiapps.controller.process.variables.Variables;
+import org.cloudfoundry.multiapps.controller.processes.metering.MicrometerNotifier;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.junit.Rule;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -72,6 +73,8 @@ public class StartProcessListenerTest {
     private ProcessLogsPersister processLogsPersister = new ProcessLogsPersister();
     @Mock
     private ApplicationConfiguration configuration;
+    @Mock
+    private MicrometerNotifier micrometerNotifier;
     @Mock
     private HistoricOperationEventPersister historicOperationEventPersister;
 

boyan-velinov/cf-mta-deploy-service

multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/listeners/EndProcessListener.java
@@ -5,6 +5,7 @@ import javax.inject.Named;
 
 import org.cloudfoundry.multiapps.controller.api.model.Operation;
 import org.cloudfoundry.multiapps.controller.process.util.OperationInFinalStateHandler;
+import org.cloudfoundry.multiapps.controller.processes.metering.MicrometerProcessesMetricsRecorder;
 import org.flowable.engine.delegate.DelegateExecution;
 
 @Named("endProcessListener")
@@ -13,16 +14,19 @@ public class EndProcessListener extends AbstractProcessExecutionListener {
     private static final long serialVersionUID = 2L;
 
     private final OperationInFinalStateHandler eventHandler;
+    private MicrometerProcessesMetricsRecorder micrometerNotifier;
 
     @Inject
-    public EndProcessListener(OperationInFinalStateHandler eventHandler) {
+    public EndProcessListener(OperationInFinalStateHandler eventHandler, MicrometerProcessesMetricsRecorder micrometerNotifier) {
         this.eventHandler = eventHandler;
+        this.micrometerNotifier = micrometerNotifier;
     }
 
     @Override
     protected void notifyInternal(DelegateExecution execution) {
         if (isRootProcess(execution)) {
             eventHandler.handle(execution, Operation.State.FINISHED);
+            micrometerNotifier.recordEndProcessEvent(execution);
         }
     }
 

multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/listeners/ErrorProcessListener.java
@@ -7,6 +7,7 @@ import org.cloudfoundry.multiapps.controller.core.util.LoggingUtil;
 import org.cloudfoundry.multiapps.controller.process.util.OperationInErrorStateHandler;
 import org.cloudfoundry.multiapps.controller.process.variables.VariableHandling;
 import org.cloudfoundry.multiapps.controller.process.variables.Variables;
+import org.cloudfoundry.multiapps.controller.processes.metering.MicrometerProcessesMetricsRecorder;
 import org.flowable.common.engine.api.delegate.event.FlowableEngineEntityEvent;
 import org.flowable.common.engine.api.delegate.event.FlowableEngineEvent;
 import org.flowable.common.engine.api.delegate.event.FlowableExceptionEvent;
@@ -22,10 +23,12 @@ public class ErrorProcessListener extends AbstractFlowableEngineEventListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(ErrorProcessListener.class);
 
     private final OperationInErrorStateHandler eventHandler;
+    private MicrometerProcessesMetricsRecorder micrometerNotifier;
 
     @Inject
-    public ErrorProcessListener(OperationInErrorStateHandler eventHandler) {
+    public ErrorProcessListener(OperationInErrorStateHandler eventHandler, MicrometerProcessesMetricsRecorder micrometerNotifier) {
         this.eventHandler = eventHandler;
+        this.micrometerNotifier = micrometerNotifier;
     }
 
     @Override
@@ -71,12 +74,15 @@ public class ErrorProcessListener extends AbstractFlowableEngineEventListener {
 
     private void handle(FlowableEngineEvent event, FlowableExceptionEvent exceptionEvent) {
         Throwable throwable = exceptionEvent.getCause();
+        DelegateExecution execution = getExecution(event);
         if (throwable == null) {
             LOGGER.error("Job execution failure detected for process \"{}\" (definition: \"{}\"), but the exception event does not contain an exception.",
                          event.getProcessInstanceId(), event.getProcessDefinitionId());
+            micrometerNotifier.recordErrorProcessEvent(execution, null);
         } else {
             LOGGER.error("Job execution failure detected for process \"{}\" (definition: \"{}\").", event.getProcessInstanceId(),
                          event.getProcessDefinitionId(), throwable);
+            micrometerNotifier.recordErrorProcessEvent(execution, throwable.getMessage());
             eventHandler.handle(event, throwable);
         }
     }

multiapps-controller-process/src/main/java/org/cloudfoundry/multiapps/controller/process/util/OperationTimeAggregator.java
@@ -8,9 +8,14 @@ import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.cloudfoundry.multiapps.controller.api.model.Operation;
 import org.cloudfoundry.multiapps.controller.process.flowable.FlowableFacade;
 import org.cloudfoundry.multiapps.controller.process.util.ProcessTimeCalculator.ProcessTime;
 import org.cloudfoundry.multiapps.controller.process.util.ProcessTimeCalculator.ProcessTimeLogger;
+import org.cloudfoundry.multiapps.controller.process.variables.VariableHandling;
+import org.cloudfoundry.multiapps.controller.process.variables.Variables;
+import org.cloudfoundry.multiapps.controller.processes.metering.MicrometerProcessesMetricsRecorder;
+import org.flowable.engine.delegate.DelegateExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,14 +26,17 @@ public class OperationTimeAggregator {
 
     private ProcessTimeCalculator processTimeCalculator;
     private FlowableFacade flowableFacade;
+    private MicrometerProcessesMetricsRecorder micrometerNotifier;
 
     @Inject
-    public OperationTimeAggregator(FlowableFacade flowableFacade) {
+    public OperationTimeAggregator(FlowableFacade flowableFacade, MicrometerProcessesMetricsRecorder micrometerNotifier) {
         this.flowableFacade = flowableFacade;
         this.processTimeCalculator = new ProcessTimeCalculator(flowableFacade);
+        this.micrometerNotifier = micrometerNotifier;
     }
 
-    public void aggregateOperationTime(String correlationId) {
+    public void aggregateOperationTime(DelegateExecution execution, Operation.State state) {
+        String correlationId = VariableHandling.get(execution, Variables.CORRELATION_ID);
         List<String> historicSubProcesses = flowableFacade.getHistoricSubProcessIds(correlationId);
         historicSubProcesses.add(correlationId);
 
@@ -39,6 +47,7 @@ public class OperationTimeAggregator {
                 key));
 
         ProcessTime rootProcessTime = processTimesForSubProcesses.get(correlationId);
+        micrometerNotifier.recordOverallTime(execution, state, rootProcessTime.getProcessDuration());
         logOverallProcesstime(correlationId, rootProcessTime, processTimesForSubProcesses.values());
     }
 

multiapps-controller-process/src/test/java/org/cloudfoundry/multiapps/controller/process/listeners/ErrorProcessListenerTest.java
@@ -5,6 +5,7 @@ import org.cloudfoundry.multiapps.controller.process.util.OperationInErrorStateH
 import org.cloudfoundry.multiapps.controller.process.util.ProcessTimeCalculator;
 import org.cloudfoundry.multiapps.controller.process.variables.VariableHandling;
 import org.cloudfoundry.multiapps.controller.process.variables.Variables;
+import org.cloudfoundry.multiapps.controller.processes.metering.MicrometerProcessesMetricsRecorder;
 import org.flowable.common.engine.api.delegate.event.FlowableEngineEntityEvent;
 import org.flowable.common.engine.api.delegate.event.FlowableEngineEvent;
 import org.flowable.common.engine.api.delegate.event.FlowableExceptionEvent;
@@ -24,8 +25,8 @@ public class ErrorProcessListenerTest {
 
     private static class ErrorProcessListenerMock extends ErrorProcessListener {
 
-        public ErrorProcessListenerMock(OperationInErrorStateHandler eventHandler) {
-            super(eventHandler);
+        public ErrorProcessListenerMock(OperationInErrorStateHandler eventHandler, MicrometerProcessesMetricsRecorder micrometerNotifier) {
+            super(eventHandler, micrometerNotifier);
         }
 
         protected DelegateExecution execution;
@@ -39,6 +40,9 @@ public class ErrorProcessListenerTest {
 
     @Mock
     private OperationInErrorStateHandler eventHandler;
+    
+    @Mock
+    private MicrometerProcessesMetricsRecorder micrometerNotifier;
 
     @Mock
     private ProcessTimeCalculator processTimeCalculator;

multiapps-controller-process/src/test/java/org/cloudfoundry/multiapps/controller/process/listeners/StartProcessListenerTest.java
@@ -25,6 +25,7 @@ import org.cloudfoundry.multiapps.controller.process.util.ProcessTypeParser;
 import org.cloudfoundry.multiapps.controller.process.util.StepLogger;
 import org.cloudfoundry.multiapps.controller.process.variables.VariableHandling;
 import org.cloudfoundry.multiapps.controller.process.variables.Variables;
+import org.cloudfoundry.multiapps.controller.processes.metering.MicrometerProcessesMetricsRecorder;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.junit.Rule;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -72,6 +73,8 @@ public class StartProcessListenerTest {
     private ProcessLogsPersister processLogsPersister = new ProcessLogsPersister();
     @Mock
     private ApplicationConfiguration configuration;
+    @Mock
+    private MicrometerProcessesMetricsRecorder micrometerNotifier;
     @Mock
     private HistoricOperationEventPersister historicOperationEventPersister;
 